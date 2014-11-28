1ffa58940855e373b3ed227e6d4a04b99f5e6b64
srfernandez <srfernandez93@gmail.com>
2014-11-28 13:31:43 +0100

    Primera version controlador de posts

--- controller/PostsController.php
+++ controller/PostsController.php
@@ -1,28 +1,65 @@
 <?php
 
 require_once(__DIR__."/../core/ViewManager.php");
-require_once(__DIR__."/../model/Posts.php");
 require_once(__DIR__."/../model/User.php");
+require_once(__DIR__."/../model/Post.php");
 require_once(__DIR__."/../controller/DBController.php");
 
 class PostsController extends DBController {
  
-  private $postMapper;    
+  private $post;   
+ 
   
   public function __construct() {    
     parent::__construct();
     
-    $this->postMapper = new Post();
+    $this->post = new Post();
+
   }
 
   public function dashboard() {
-       $posts =$this->postMapper->findAll();
-	   
-	
-    
-    $this->view->render("posts", "dashboard");    
+	$currentuser=$_SESSION["currentuser"];
+	$posts =$this -> post ->findPosts($currentuser);
+	 if ($posts == NULL) {
+      throw new Exception("No se encontro ningun amigo de: ".$currentuser->getUsername());
+    }
+	$this->view->setVariable("posts", $posts);
+	$this->view->render("posts","dashboard");
   }
   
+	public function newPost(){
+		$posts= new Post();
+		$currentuser=$_SESSION["currentuser"];
+		if (isset($_POST["content"])){ // reaching via HTTP Post...
+		  $posts->setContent($_POST["content"]);
+		  $posts->setAuthor($currentuser);
+		  
+		  try{
+			
+			if ($posts->validFormat(); ){
+			  $this->post->save($post);
+			 
+			  $this->view->redirect("posts", "dashboard");	  
+			} else {
+			  $errors = array();
+			  $errors["general"] = "Error al publicar el post";
+			  $this->view->setVariable("errors", $errors);
+			}
+		  }catch(ValidationException $ex) {
 
+		$errors = $ex->getErrors();
+
+		$this->view->setVariable("errors", $errors);
+		  }
+		}
+		
+		$this->view->setVariable("posts", $posts);
+		
+		$this->view->render("posts", "dashboard");
+		
+	  }
+	
+	}
+   
   
 }
